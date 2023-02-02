ce076e52c199610d28b45424c6e98013009cc260
Aayush042003 <117824761+Aayush042003@users.noreply.github.com>
2023-02-02 10:36:06 +0530

    Module 4 Assignment

--- Module-4 Solution/script.js
+++ Module-4 Solution/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Aayush", "Ashish", "Joker", "Jason", "Paul", "Frank", "Ishita", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
